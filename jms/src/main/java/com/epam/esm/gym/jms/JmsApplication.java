3d17a36fa5518ba4cfc04c25fe6bd058613d6113
Fat-Frumos <lokankara@i.ua>
2024-09-28 18:09:21 +0300

    Update dockerfile

--- jms/src/main/java/com/epam/esm/gym/jms/JmsApplication.java
+++ jms/src/main/java/com/epam/esm/gym/jms/JmsApplication.java
@@ -1,7 +1,8 @@
 package com.epam.esm.gym.jms;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
 
 /**
  * Main entry point for the JmsApplication application.
@@ -9,17 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * for the Jms service. The application is initialized with default settings
  * and configurations defined in the classpath.
  */
-@SpringBootApplication
+@Slf4j
 public class JmsApplication {
     /**
      * Main method to run the Spring Boot application.
-     * This method is the entry point for the application. It launches the Spring Boot
-     * application by calling {@link SpringApplication#run(Class, String...)} with the
+     * This method is the entry point for the application.
      * {@link JmsApplication} class and any provided command-line arguments.
      *
      * @param args command-line arguments passed to the application.
      */
     public static void main(String[] args) {
-        SpringApplication.run(JmsApplication.class, args);
+        log.info(Arrays.toString(args));
     }
 }
